328b78859311e488190b6f7f6bab0d3d44611c46
Gabriel Quintiliano <gabrielq.pereira@gmail.com>
2023-08-20 12:27:09 -0300

    [completed] Approch using a global object for all the form items

--- 2-programming-logic-JS-TS/final-challenge/cadastro.js
+++ 2-programming-logic-JS-TS/final-challenge/cadastro.js
@@ -1,32 +1,19 @@
 const RegisteredItemsWrapper = document.querySelector("#registered-items-wrapper");
-// const formElems = [document.querySelector("#prod-code"),
-//                    document.querySelector("#prod-name"),
-//                    document.querySelector("#prod-qty"),
-//                    document.querySelector("#prod-un-price")]
-const prodCode = document.querySelector("#prod-code");
-const prodName = document.querySelector("#prod-name");
-const prodQty = document.querySelector("#prod-qty");
-const prodUnPrice = document.querySelector("#prod-un-price");
+const formElems = {prodCode: document.querySelector("#prod-code-input"),
+                   prodName: document.querySelector("#prod-name-input"),
+                   prodQty: document.querySelector("#prod-qty-input"),
+                   prodUnPrice: document.querySelector("#prod-un-price-input")}
+// const prodCode = document.querySelector("#prod-code");
+// const prodName = document.querySelector("#prod-name");
+// const prodQty = document.querySelector("#prod-qty");
+// const prodUnPrice = document.querySelector("#prod-un-price");
 
 const prodInfoForm = document.querySelector("#prod-info-form")
 
 prodInfoForm.addEventListener("submit", (e) => {
 
-    e.preventDefault();
-    const newItem = parseFormInfo();
-    createNewItemRegister(newItem);
+    createNewItemRegister(formElems);
 })
-
-function parseFormInfo() {
-
-    let newItemObject = {prodCode: prodCode.value,
-                         prodName: prodName.value,
-                         prodQty: prodQty.value,
-                         prodUnPrice: prodUnPrice.value,
-                         total: parseInt(prodUnPrice.value) * parseInt(prodQty.value)};
-
-    return newItemObject;
-}
 
 function createNewItemRegister(newItemObject) {
 
@@ -35,13 +22,23 @@ function createNewItemRegister(newItemObject) {
 
     let newTd;
 
-    for (let [ key, value ] of Object.entries(newItemObject)) {
+    for (let [ key, itemInput ] of Object.entries(newItemObject)) {
 
         newTd = document.createElement("td");
-        newTd.innerText = value;
-
+        newTd.textContent = itemInput.value;
+        newTd.setAttribute("class", key.replace("-input", ""));
+        
         newTableRow.appendChild(newTd);
         // console.log(`key = ${key}: ${value}`)
+    }
+    
+    newTd = document.createElement("td");
+    newTd.textContent = parseInt(formElems.prodUnPrice.value) * parseInt(formElems.prodQty.value);
+    newTd.setAttribute("class", "prod-total");
+    newTableRow.appendChild(newTd);
+
+    for (let itemInput of Object.entries(formElems)) {
+        itemInput.value = "";
     }
 
     RegisteredItemsWrapper.appendChild(newTableRow)
