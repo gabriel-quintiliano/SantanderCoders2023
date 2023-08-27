46eb3e25a19cd143978c43e9aba3612dc5c5aab5
Gabriel Quintiliano <gabrielq.pereira@gmail.com>
2023-08-27 18:55:23 -0300

    implemented getApothem and getArea static methods for RegularPolygon class

--- 3-object-oriented-programming-JS-TS/class-1/Polygon.ts
+++ 3-object-oriented-programming-JS-TS/class-1/Polygon.ts
@@ -88,6 +88,51 @@ class RegularPolygon extends Polygon {
         const result: number = 180 * sides - 360;
         return Number(result.toFixed(2))
     }
+
+    static getArea(sides: number, sideLen: number): number{
+        // Area = (Perimeter * apothem) / 2
+        // Area = (sideLen*sides * apothem) / 2
+        
+        let apothem: number = RegularPolygon.getApothem(sides, sideLen);
+        let polArea: number = (sideLen * sides * apothem) / 2
+    
+        // returns the area with up to 3 decimal digits
+        return Number(polArea.toFixed(3))
+    }
+    
+    static getApothem(sides: number, sideLen: number): number{
+        // Apothem is the perpendicular segment between any side of a regular polygon and the center
+        // of the circle that circumscribes that polygon. Regular polygons can be sectioned into
+        // identical triangles with the following characteristics:
+        //  -  Base length = polygon's side length
+        //  -  Isosceles, with base angles = half the polygon's internal angle
+        //  -  The isosceles sides = radius of the circle that circumscribes that polygon
+        //  -  Heigh = the polygon's apothem
+        // Thus, we can get the apothem calculating this triangles height
+        //         .    
+        //        /|\          ap = apothem = triangle height
+        // c.rad / | \          x = polygon's internal angle / 2
+        //      /ap|  \ c.rad --> = circumcircle radius
+        //     /x__|__x\
+        //      sideLen
+        //
+        // Now, as we only have the base and one angle of the isosceles triangle, what we can do is
+        // to look at the rectangular triangle formed by 'c. rad', 'ap' and 'sideLen / 2' and this way
+        // get the length of 'ap' through the tangent of 'x' as 'tg = opposite cath. / adjacent cath'
+        // so...
+        // tg x = ap / (sideLen / 2)
+        // tg (pol's int. angle / 2) = ap / (sideLen / 2)
+        // tg ((pol's int angle sum / sides) / 2) = ap / (sideLen / 2)
+        // tg ((((sides - 2) * 180) / sides) / 2) = ap / (sideLen / 2)
+        // ap = (tg ((((sides - 2) * 180) / sides) / 2)) * (sideLen / 2)
+    
+        let x: number = (((sides - 2) * 180) / sides) / 2;
+        let xInRad: number = (x * Math.PI) / 180; // converted angle x to radians as it's the format of Math.tan()
+        let tanX: number = Math.tan(xInRad);
+        let apothem: number = tanX * (sideLen / 2);
+    
+        return apothem;
+    }
 }
 
 class Square extends RegularPolygon {
@@ -108,10 +153,16 @@ class RegularPentagon extends RegularPolygon {
     }
 }
 
-const mySquare = new Square();
-const myRegularTriangle = new RegularTriangle();
-const myRegularPentagon = new RegularPentagon();
+// const mySquare = new Square();
+// const myRegularTriangle = new RegularTriangle();
+// const myRegularPentagon = new RegularPentagon();
 
-console.log(`this is a ${mySquare}`);
-console.log(`${myRegularPentagon.name} has ${myRegularPentagon.sides} with internal angles of ${myRegularPentagon.intAngle} and external angles of ${myRegularPentagon.extAngle}`);
-console.log(`A 21-sided regular polygon has internal angles of ${RegularPolygon.getIntAngle(21)} degrees`)
+// console.log(`this is a ${mySquare}`);
+// console.log(`${myRegularPentagon.name} has ${myRegularPentagon.sides} with internal angles of ${myRegularPentagon.intAngle} and external angles of ${myRegularPentagon.extAngle}`);
+// console.log(`A 21-sided regular polygon has internal angles of ${RegularPolygon.getIntAngle(21)} degrees`)
+
+console.log(`Area of a 4-sided polygon with side 2 is: ${RegularPolygon.getArea(4,2)}`)
+console.log(`Area of a 4-sided polygon with side 3 is: ${RegularPolygon.getArea(4,3)}`)
+console.log(`Area of a 4-sided polygon with side 4 is: ${RegularPolygon.getArea(4,4)}`)
+console.log(`Area of a 5-sided polygon with side 4 is: ${RegularPolygon.getArea(5,4)}`)
+console.log(`Area of a 6-sided polygon with side 10 is: ${RegularPolygon.getArea(6,10)}`)
