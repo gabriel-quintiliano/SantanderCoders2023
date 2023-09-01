2e9174c421fcd8f3619dcd6b5f8ab1fa4cfdcee6
Gabriel Quintiliano <gabrielq.pereira@gmail.com>
2023-08-31 22:37:44 -0300

    Created class ContaPoupanca (which extends ContaBancaria) and implemented 4 new methods for ContaBancaria class

--- 3-object-oriented-programming-JS-TS/class-4/ContaBancaria.ts
+++ 3-object-oriented-programming-JS-TS/class-4/ContaBancaria.ts
@@ -1,13 +1,42 @@
 class ContaBancaria {
 
     titular: string;
-    numeroConta: string;
-    saldo: number;
+    private numeroConta: string;
+    private saldo: number;
 
-    constructor(titular, numeroConta, saldo) {
+    constructor(titular: string, numeroConta: string, saldoInicial: number) {
         this.titular = titular;
         this.numeroConta = numeroConta;
-        this.saldo = saldo;
+        this.saldo = saldoInicial;
     }
 
+    getSaldo() {
+        return this.saldo
+    }
+
+    consultarSaldo() {
+        return this.saldo
+    }
+
+    depositar(valor: number) {
+        this.saldo += valor;
+    }
+
+    sacar(valor: number) {
+        if (valor > this.saldo) return // implementar --> throw new SaldoInsuficiente()
+        this.saldo -= valor;
+    }
+}
+
+class ContaPoupanca extends ContaBancaria {
+
+    juros: number;
+
+    constructor(titular: string, numeroConta: string, saldoInicial: number) {
+        super(titular, numeroConta, saldoInicial);
+    }
+
+    consultarSaldo() {
+        return this.getSaldo();
+    }
 }
