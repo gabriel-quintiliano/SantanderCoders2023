40506c2e0d4f9da9ac950515ce1371ba75ae4eac
Gabriel Quintiliano <gabrielq.pereira@gmail.com>
2023-09-13 00:09:53 -0300

    Added attribute id in class Player and refactored inner constructor logic of class ComputerPlayer

--- 3-object-oriented-programming-JS-TS/hangman-game-final-project/src/Players.ts
+++ 3-object-oriented-programming-JS-TS/hangman-game-final-project/src/Players.ts
@@ -2,12 +2,16 @@ interface IPlayer {
     guessLetter(letter: string): boolean;
 }
 
-class Player {
+export default class Player {
+    private static classInstances: number = 0;
     name: string;
     score: number = 0;
+    id: number;
 
     constructor(name: string) {
         this.name = name;
+        Player.classInstances++;
+        this.id = Player.classInstances;
     }
 }
 
@@ -22,8 +26,9 @@ export class HumanPlayer extends Player implements IPlayer {
 }
 
 export class ComputerPlayer extends Player implements IPlayer {
-    constructor(name: string) {
-        super(name);
+    constructor() {
+        super(`Computer`);
+        this.name = `${this.name}ID${this.id}`
     }
     
     guessLetter(letter: string): boolean{
