fd27ecd38ecb3444d74a9ef5d081173677e7d22c
Gabriel Quintiliano <gabrielq.pereira@gmail.com>
2023-09-13 20:06:15 -0300

    Updated HangmanGameLogic.ts

--- 3-object-oriented-programming-JS-TS/hangman-game-final-project/src/HangmanGameLogic.ts
+++ 3-object-oriented-programming-JS-TS/hangman-game-final-project/src/HangmanGameLogic.ts
@@ -1,61 +1,45 @@
 import * as readline from 'readline-sync';
 import SecretWord, { LetterMatches} from "./SecretWord.js";
-import Player, { HumanPlayer, ComputerPlayer, IPlayer } from "./Players.js";
+import { HumanPlayer, ComputerPlayer, IPlayer } from "./Players.js";
 import GameRenderCLI from "./GameRenderCLI.js";
 
 type PlayerScores = {
     [playerName: string]: string;
 };
 
+enum difficultyRating {
+    easy = 0,
+    medium = 1,
+    hard = 2,
+    veryHard = 3
+}
+
 class HangmanGameLogic {
-    player?: IPlayer;
-    secretWord?: SecretWord;
-    difficult?: number;
+    humanPlayer: HumanPlayer;
+    computerPlayer: ComputerPlayer;
+    curPlayer: IPlayer;
+    secretWord: SecretWord;
+    difficulty: number;
     guessedLetters: number = 0;
     guessingsLeft: number = 6;
     scoreBoard: PlayerScores = {};
-    gameRenderCLI?: GameRenderCLI;
+    gameRenderCLI: GameRenderCLI;
 
     constructor() {
-        let playerOption;
-        let difficultyOption;
+        this.humanPlayer = new HumanPlayer;
+        this.curPlayer = this.humanPlayer; // Human plays first always
+        this.computerPlayer = new ComputerPlayer;
         
         do {
-            playerOption = Number(readline.question("Um novo jogo vai começar, quem vai jogar? [0 - Humano | 1 - Computador]\n"));
-        } while (playerOption < 0 || playerOption > 1 )
+            this.difficulty = Number(readline.question("Digite a dificuldade desejada? [0 - Fácil | 1 - Regular | 2 - Difícil | 3 - Muito Difício]\n"));
+        } while (this.difficulty < difficultyRating.easy || this.difficulty > difficultyRating.veryHard )
 
-        do {
-            difficultyOption = Number(readline.question("Digite a dificuldade desejada? [0 - Fácil | 1 - Regular | 2 - Difícil | 3 - Desafio]\n"));
-        } while (difficultyOption < 0 || difficultyOption > 3 )
-
-        switch (playerOption) {
-            case 0:
-                this.initGameForHumanPlayer();
-            case 1:
-                this.initGameForComputerPlayer();
-        }
-
-        // Implementar uma maneira de escolher a dificuldade do jogo
-
-        this.secretWord = new SecretWord(this.difficult); // passar dificuldade aqui
+        this.secretWord = new SecretWord(this.difficulty); // passar dificuldade aqui
         this.gameRenderCLI = new GameRenderCLI();
-
-        
-
     }
 
     private askForPlayerGuessing(): LetterMatches {
-        let userInput: string = readline.question("Guessing (letter): ");
-        let matches: LetterMatches = this.player?.guessLetter(userInput[0], this.secretWord);
-        return matches;
+        return this.curPlayer.guessLetter(this.secretWord);
     }
 
-    private initGameForHumanPlayer() {
-        let playerName = readline.question("Digite seu nome:\n")
-        this.player = new HumanPlayer(playerName);
-    }
-
-    private initGameForComputerPlayer() {
-        this.player = new ComputerPlayer();
-    }
 }
