bf4c069b279c11596887bccba349066bd8aae7b9
Gabriel Quintiliano <115936067+gabriel-quintiliano@users.noreply.github.com>
2023-09-13 16:58:18 -0300

    Replaced the constructor function for method startGame, added attributes 'difficulty' and 'gameRenderCLI', implemented some other important private methods

--- 3-object-oriented-programming-JS-TS/hangman-game-final-project/src/HangmanGameLogic.ts
+++ 3-object-oriented-programming-JS-TS/hangman-game-final-project/src/HangmanGameLogic.ts
@@ -1,30 +1,53 @@
 import * as readline from 'readline-sync';
-import SecretWord from "./SecretWord.js";
+import SecretWord, { LetterMatches} from "./SecretWord.js";
 import Player, { HumanPlayer, ComputerPlayer } from "./Players.js";
+import GameRenderCLI from "./GameRenderCLI.js";
 
 type PlayerScores = {
     [playerName: string]: string;
 };
+
 class HangmanGameLogic {
     player?: Player;
     secretWord?: SecretWord;
+    difficult?: number;
     guessedLetters: number = 0;
     guessingsLeft: number = 6;
     scoreBoard: PlayerScores = {};
+    gameRenderCLI?: GameRenderCLI;
 
-    constructor() {
-        let playerOption: number = Number(readline.question("Um novo jogo vai começar, quem vai jogar? [1 - Humano | 2 - Computador]\n"));
+    startGame() {
+        let playerOption;
+        let difficultyOption;
         
-        while (playerOption < 1 || playerOption > 2 ) {
-            playerOption = Number(readline.question("Um novo jogo vai começar, quem vai jogar? [1 - Humano | 2 - Computador]\n"));
-        }
+        do {
+            playerOption = Number(readline.question("Um novo jogo vai começar, quem vai jogar? [0 - Humano | 1 - Computador]\n"));
+        } while (playerOption < 0 || playerOption > 1 )
+
+        do {
+            difficultyOption = Number(readline.question("Digite a dificuldade desejada? [0 - Fácil | 1 - Regular | 2 - Difícil | 3 - Desafio]\n"));
+        } while (difficultyOption < 0 || difficultyOption > 3 )
 
         switch (playerOption) {
-            case 1:
+            case 0:
                 this.initGameForHumanPlayer();
-            case 2:
+            case 1:
                 this.initGameForComputerPlayer();
         }
+
+        // Implementar uma maneira de escolher a dificuldade do jogo
+
+        this.secretWord = new SecretWord(this.difficult); // passar dificuldade aqui
+        this.gameRenderCLI = new GameRenderCLI();
+
+        
+
+    }
+
+    private askForPlayerGuessing(): LetterMatches {
+        let userInput: string = readline.question("Guessing (letter): ");
+        let matches: LetterMatches = this.player.guessLetter(userInput[0]);
+        return matches;
     }
 
     private initGameForHumanPlayer() {
