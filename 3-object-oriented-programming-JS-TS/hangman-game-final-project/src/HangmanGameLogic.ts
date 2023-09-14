679da1bf3d31b19ba4c53a98cb44bc112867e1ae
Gabriel Quintiliano <115936067+gabriel-quintiliano@users.noreply.github.com>
2023-09-14 16:19:54 -0300

    Removed attributes 'secretWord', 'curPlayer' - logic broken, work in progress to adapt this to work with new class 'StandaloneGameMatch'

--- 3-object-oriented-programming-JS-TS/hangman-game-final-project/src/HangmanGameLogic.ts
+++ 3-object-oriented-programming-JS-TS/hangman-game-final-project/src/HangmanGameLogic.ts
@@ -1,5 +1,3 @@
-// Não consegui terminar por completo ainda
-
 import * as readline from 'readline-sync';
 import SecretWord, { LetterMatches} from "./SecretWord.js";
 import { HumanPlayer, ComputerPlayer, IPlayer } from "./Players.js";
@@ -23,8 +21,6 @@ enum initGameOptions {
 
 class HangmanGameLogic {
     players: Array<IPlayer>;
-    curPlayer: IPlayer;
-    secretWord: SecretWord;
     difficulty: number;
     guessedLetters: number = 0;
     guessingsLeft: number = 6;
@@ -35,19 +31,31 @@ class HangmanGameLogic {
         console.log(`Um novo jogo da forca está preste a começar, digite o nome de cada jogador conforme solicitado:\n(- Digite '${initGameOptions.computerPlayer}')\n(- Digite '${initGameOptions.done}' para salvar os jogadores)\n`);
         this.players = this.initPlayersArray();
         
-        let randPlayerIndex = Math.floor(Math.random() * this.players.length)
-        this.curPlayer = this.players[randPlayerIndex]
-        
         do {
             this.difficulty = readline.questionInt("Digite a dificuldade desejada? [0 - Fácil | 1 - Regular | 2 - Difícil | 3 - Muito Difício]\n");
         } while (this.difficulty < difficultyRatings.easy || this.difficulty > difficultyRatings.veryHard )
 
-        this.secretWord = new SecretWord(this.difficulty);
         this.gameRenderCLI = new GameRenderCLI();
     }
 
-    startGame() {
+    startTournament() {
+        const firstToPlayIndex = Math.floor(Math.random() * this.players.length);
+        let curPlayer: IPlayer = this.players[firstToPlayIndex];
 
+        let curPlayerIndex: number = firstToPlayIndex;
+        let nextPlayerIndex: number;
+
+        do {
+            nextPlayerIndex = (curPlayerIndex + 1) % this.players.length; 
+            startGameForPlayer(curPlayer);
+
+            curPlayerIndex = nextPlayerIndex;
+        } while (nextPlayerIndex != firstToPlayIndex) // The loop will go on until every player has player (and we're back to the player 'firstToPlay')
+    }
+
+    private startGameForPlayer(player: IPlayer) {
+        const secretWord = new SecretWord(this.difficulty);
+        
     }
 
     private initPlayersArray(): Array<IPlayer> {
@@ -74,8 +82,7 @@ class HangmanGameLogic {
         }
     }
 
-    private askForPlayerGuessing(): LetterMatches {
-        return this.curPlayer.guessLetter(this.secretWord);
+    private askForPlayerGuessing(player: IPlayer): LetterMatches {
+        return player.guessLetter(this.secretWord);
     }
-
 }
