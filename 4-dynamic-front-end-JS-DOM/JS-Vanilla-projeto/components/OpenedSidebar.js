11109d74cd8371a29ebc3ade2fa24ce7fb1d8c8d
Gabriel Quintiliano <gabrielq.pereira@gmail.com>
2023-09-28 21:03:33 -0300

    components/OpenedSidebar.js: Create initial structure

---
+++ 4-dynamic-front-end-JS-DOM/JS-Vanilla-projeto/components/OpenedSidebar.js
@@ -0,0 +1,32 @@
+export function renderSidebar() {
+    const styleTag = document.createElement("style");
+
+    styleTag.innerHTML = `
+        dd
+    `;
+
+    // Obtendo a tag head do HTML
+    const headTag = document.querySelector("head");
+
+    // Inserindo a tag style como filha da tag head
+    headTag.appendChild(styleTag);
+
+    return `
+        <nav>
+            <ul>
+                <li>Overview</li>
+                <li>
+                    Period
+                    <ul>
+                        <li>Day</li>
+                        <li>Month</li>
+                        <li>Year</li>
+                    </ul>
+                </li>
+                <li>Chart</li>
+                <li>Calendar</li>
+                <li>Messages</li>
+            </ul>
+        </nav>  
+    `
+}
